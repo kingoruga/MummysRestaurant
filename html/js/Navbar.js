a591d480a70ab2ef44789a7c760621e22937f9b1
ashrua <avannie1@asu.edu>
2018-01-23 15:16:53 -0700

    Update Navbar.js

--- html/js/Navbar.js
+++ html/js/Navbar.js
@@ -10,13 +10,13 @@ NavBar = React.createClass({
     {
         return (
         <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-dark">
-            <a className="navbar-brand" href="#">Mummy's Restaurant</a>
-            <a className="nav-link text-light" href="#">View Menu</a>
-            <a className="nav-link text-light" href="#">My Orders(0)</a>
-            <a className="nav-link text-light" href="#">Manage Users</a>
-            <a className="nav-link text-light" href="#">Manage Areas</a>
-            <a className="nav-link text-light" href="#">Manage Packages</a>
-            <a className="nav-link text-light" href="#">Manage Orders</a>
+            <a className="navbar-brand" href="Home.html">Mummy's Restaurant</a>
+            <a className="nav-link text-light" href="viewMenu.html">View Menu</a>
+            <a className="nav-link text-light" href="myOrders.html">My Orders(0)</a>
+            <a className="nav-link text-light" href="manageUsers.html">Manage Users</a>
+            <a className="nav-link text-light" href="manageAreas.html">Manage Areas</a>
+            <a className="nav-link text-light" href="managePackages.html">Manage Packages</a>
+            <a className="nav-link text-light" href="manageOrders.html">Manage Orders</a>
             <a className="nav-link text-light" href="#">Logout</a>
         </nav>
         );
@@ -25,9 +25,9 @@ NavBar = React.createClass({
     {
         return (
         <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-dark">
-            <a className="navbar-brand" href="#">Mummy's Restaurant</a>
-            <a className="nav-link text-light" href="#">View Menu</a>
-            <a className="nav-link text-light" href="#">My Orders(0)</a>
+            <a className="navbar-brand" href="Home.html">Mummy's Restaurant</a>
+            <a className="nav-link text-light" href="viewMenu.html">View Menu</a>
+            <a className="nav-link text-light" href="myOrders.html">My Orders(0)</a>
             <a className="nav-link text-light" href="#">Logout</a>
         </nav>
         );
@@ -36,8 +36,8 @@ NavBar = React.createClass({
     {
         return (
         <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-dark">
-            <a className="navbar-brand" href="#">Mummy's Restaurant</a>
-            <a className="nav-link text-light" href="#">View Menu</a>
+            <a className="navbar-brand" href="Home.html">Mummy's Restaurant</a>
+            <a className="nav-link text-light" href="viewMenu.html">View Menu</a>
             <a className="nav-link text-light" href="#">Login</a>
             <a className="nav-link text-light" href="#">Register</a>
         </nav>
